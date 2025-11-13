6442dfa64ee7665d6252d500a0b4220cf9163934
Lord-DanielVieira <2432132@hc.ac.je>
2025-11-13 11:39:06 +0000

    theme

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { RiTwitterXFill, RiLinkedinBoxFill } from 'react-icons/ri'
 import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
@@ -6,9 +7,9 @@ import headshot2 from './assets/jpg/portrait-02.jpg'
 import headshot3 from './assets/jpg/portrait-03.jpg'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
+import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
-import { RiTwitterXFill, RiLinkedinBoxFill } from 'react-icons/ri'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -47,9 +48,10 @@ export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
+	const theme = useTheme()
 
 	return (
-		<Document nonce={nonce} honeyProps={data?.honeyProps}>
+		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
@@ -75,6 +77,9 @@ export default function App() {
 							/>
 						</div>
 					</main>
+				</div>
+				<div className="container flex justify-between pb-5">
+					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
 				</div>
 			</div>
 		</Document>
