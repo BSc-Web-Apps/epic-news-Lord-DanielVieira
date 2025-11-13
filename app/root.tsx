061e5b19d363b99be4b64f65b9db0dc0a0d34612
Lord-DanielVieira <2432132@hc.ac.je>
2025-11-13 10:19:21 +0000

    TEAMMEMBER CARD

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,9 @@
 import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
-import portrait1 from './assets/jpg/portrait-01.jpg'
+import headshot from './assets/jpg/portrait-01.jpg'
+import headshot2 from './assets/jpg/portrait-02.jpg'
+import headshot3 from './assets/jpg/portrait-03.jpg'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
@@ -14,6 +16,34 @@ export const links: Route.LinksFunction = () => {
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
 
+interface TeamMemberCardProps {
+	name: string
+	role: string
+	imageSrc: string
+}
+
+export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
+	return (
+		<div className="w-fit rounded-lg bg-slate-800 p-8">
+			<img
+				src={imageSrc}
+				alt="An employee"
+				className="mx-auto h-64 w-64 rounded-full"
+			/>
+
+			<div className="pt-6">
+				<h3 className="font-semi-bold text-center text-white">{name}</h3>
+				<p className="pt-1 text-center text-slate-400">{role}</p>
+
+				<div className="flex justify-center gap-4 pt-6 text-slate-400">
+					<RiTwitterXFill />
+					<RiLinkedinBoxFill />
+				</div>
+			</div>
+		</div>
+	)
+}
+
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
@@ -25,26 +55,23 @@ export default function App() {
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Your journey begins</h1>
 
-						<div className="w-fit rounded-lg bg-slate-800 p-8">
-							<img
-								src={portrait1}
-								alt="Portrait 1"
-								className="mt-8 w-48 rounded-full shadow-lg md:w-64 lg:w-80"
-							/>
-							<div className="pt-6">
-								<h3 className="font-semi-bold text-center text-white">
-									Leonard Krasner
-								</h3>
-								<p className="pt-1 text-center text-slate-400">
-									Senior Designer
-								</p>
+						<TeamMemberCard
+							name="Leonard Krasner"
+							role="Senior Designer"
+							imageSrc={headshot}
+						/>
 
-								<div className="flex justify-center gap-4 pt-6 text-slate-400">
-									<RiTwitterXFill />
-									<RiLinkedinBoxFill />
-								</div>
-							</div>
-						</div>
+						<TeamMemberCard
+							name="JOHN SNOW"
+							role="LEVEL Designer"
+							imageSrc={headshot2}
+						/>
+
+						<TeamMemberCard
+							name="JOHN PORK"
+							role="LEVEL MAKER"
+							imageSrc={headshot3}
+						/>
 					</main>
 				</div>
 			</div>
