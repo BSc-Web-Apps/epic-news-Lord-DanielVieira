2df6778c7f0838157ce68805dac75ee565d4b194
Lord-DanielVieira <2432132@hc.ac.je>
2025-11-27 12:13:34 +0000

    yes

--- app/routes/news.$category.tsx
+++ app/routes/news.$category.tsx
@@ -1,3 +1,13 @@
+import { type LoaderFunctionArgs, data } from 'react-router'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+	const { category } = params
+
+	console.log({ category })
+
+	return data({})
+}
+
 export default function NewsCategoryPage() {
 	return (
 		<div className="container py-16">
