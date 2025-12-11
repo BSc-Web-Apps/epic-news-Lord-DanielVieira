97414684c5377806c5aa95a3752240b585d6fd87
Lord-DanielVieira <152405836+Lord-DanielVieira@users.noreply.github.com>
2025-12-11 09:23:11 +0000

    Update app/routes/news.tsx
    Co-authored-by: Ged Sargent <120935232+GedSargent@users.noreply.github.com>

--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -18,7 +18,7 @@ export default function newsPage() {
 					</NavLink>
 
 					<NavLink
-						to="technology"
+						to="entertainment"
 						prefetch="intent"
 						className={({ isActive }) =>
 							`${isActive ? 'text-blue-300 underline underline-offset-8' : 'text-blue-500'}`
